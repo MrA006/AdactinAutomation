1d15bc5e201630a303f67251e38164c6e4369fd5
MrA006 <anusabid13@gmail.com>
2026-03-29 21:39:00 +0500

    Add postive case 5

--- tests/Positive_Case_05-BookWithMastercard.spec.js
+++ tests/Positive_Case_05-BookWithMastercard.spec.js
@@ -1,19 +1,14 @@
 const { test, expect } = require('@playwright/test');
 
-
-
-test('end-to-end booking flow', async ({ page }) => {
+test('book with mastercard', async ({ page }) => {
     test.slow();
 
     await page.goto('https://adactinhotelapp.com/');
-    
     await page.fill('#username', 'haristester01');
     await page.fill('#password', 'haristester01');
     await page.click('#login');
 
-    
     await page.waitForSelector('#location');
-
     await page.selectOption('#location', 'Sydney');
     await page.selectOption('#hotels', 'Hotel Cornice');
     await page.selectOption('#room_type', 'Double');
@@ -24,35 +19,23 @@ test('end-to-end booking flow', async ({ page }) => {
     await page.selectOption('#child_room', '1 - One');
     await page.click('#Submit');
 
-
     await page.click('#radiobutton_0');
     await page.click('#continue');
-    
-    await page.waitForSelector('#first_name');
-    await page.fill('#first_name','Anas');
-    await page.fill('#last_name','Arif');
-    await page.fill('#address','9843u, university road, xinjiang');
-    await page.fill('#cc_num','4758934758934567');
-    await page.selectOption('#cc_type','VISA');
-    await page.selectOption('#cc_exp_month','March');
-    await page.selectOption('#cc_exp_year','2026');
-    await page.fill('#cc_cvv','093');
 
+    await page.waitForSelector('#first_name');
+    await page.fill('#first_name', 'Anas');
+    await page.fill('#last_name', 'Arif');
+    await page.fill('#address', '9843u, university road, xinjiang');
+    await page.fill('#cc_num', '5258934758934567');
+    await page.selectOption('#cc_type', 'Master Card');
+    await page.selectOption('#cc_exp_month', 'March');
+    await page.selectOption('#cc_exp_year', '2026');
+    await page.fill('#cc_cvv', '093');
     await page.click('#book_now');
 
     await page.waitForSelector('#order_no');
     const orderNumber = await page.inputValue('#order_no');
     expect(orderNumber).toBeTruthy();
-
-    await page.click('#my_itinerary');
-    await page.waitForSelector('#order_id_text');
-    await page.fill('#order_id_text', orderNumber);
-    await page.click('#search_hotel_id');
-
-    await page.waitForSelector('#search_result_error');
-    const orderId = await page.inputValue('.select_text');
-    expect(orderId).toBe(orderNumber);
-
 
     await page.click('#logout');
     const logoutMsg = page.locator('.reg_success');
